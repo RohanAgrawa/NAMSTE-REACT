928e95b63be78eb941949cebb754c339030ba688
Rohan <rohanagrawal8989@gmail.com>
2024-06-30 14:15:09 +0530

    First Episode Learning

---
+++ App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement("h1", {id : "heading"}, "Hello world from react");
+
+//    const parent = React.createElement("div", {id : "parent"},
+//     React.createElement("h1", {id : "heading"}, "Hello from h1")  // For single div
+//    );
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child1" },
+    React.createElement("h1", { id: "heading" }, "Hello from h1")
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    React.createElement("h2", { id: "heading" }, "Hello from h2")
+  ),
+]); // Nested tags
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
